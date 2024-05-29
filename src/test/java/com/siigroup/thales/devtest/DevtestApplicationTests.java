bb5424eab2bfad925f3631c5944f107146ff5cc7
Juank544 <camilo.gv@live.com>
2024-05-29 10:59:26 -0500

    feat(): base code

---
+++ src/test/java/com/siigroup/thales/devtest/DevtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siigroup.thales.devtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
