802db1cddfbe9d2358b155cbe9ffad34248c3a81
Juank544 <camilo.gv@live.com>
2024-05-30 03:14:53 -0500

    feat(): ViewController impl

---
+++ src/main/java/com/siigroup/thales/devtest/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.siigroup.thales.devtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+}
