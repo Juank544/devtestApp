053ddd90607a898db25e709e8d3745bd82a62420
Juank544 <camilo.gv@live.com>
2024-05-30 06:49:29 -0500

    feat(): impl mapper method

--- src/main/java/com/siigroup/thales/devtest/service/DataService.java
+++ src/main/java/com/siigroup/thales/devtest/service/DataService.java
@@ -1,10 +1,12 @@
 package com.siigroup.thales.devtest.service;
 
 import com.siigroup.thales.devtest.model.dto.DummyDTO;
+import com.siigroup.thales.devtest.model.entity.Employee;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -25,5 +27,12 @@ public class DataService {
     public DummyDTO getEmployeeById(Integer id) throws HttpStatusCodeException {
         Optional<DummyDTO> optional = Optional.ofNullable(restTemplate.getForObject(apiUrl + "/employee/" + id, DummyDTO.class));
         return optional.orElseThrow();
+    }
+
+    public Employee convertLinkHasMapToEmployee(Map<String, Object> hashMap){
+        return Employee.builder().id((Integer) hashMap.get("id")).employee_name((String) hashMap.get("employee_name"))
+                .employee_salary((Integer) hashMap.get("employee_salary"))
+                .employee_age((Integer) hashMap.get("employee_age"))
+                .profile_image((String) hashMap.get("profile_image")).build();
     }
 }
